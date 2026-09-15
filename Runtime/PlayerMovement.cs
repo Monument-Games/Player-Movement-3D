29301708061861554486fdc7342f989d1fc1dda3
louisa <livesi5e@gmail.com>
2026-09-15 12:36:35 +0100

    typo

--- Runtime/PlayerMovement.cs
+++ Runtime/PlayerMovement.cs
@@ -39,7 +39,7 @@ namespace MonumentGames.PlayerMovement3D
             float movSpeed = 0;
 
             // Look if Sprint Key is pressed and set movementSpeed accordingly
-            if (sprintAction.ReadValuey<bool>())
+            if (sprintAction.ReadValue<bool>())
                 movSpeed = sprintSpeed;
             else
                 movSpeed = movementSpeed;
